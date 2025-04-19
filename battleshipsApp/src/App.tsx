2c2f2b3351548d4cdd04bfe176dde277c428cfd1
Jishnu Raychaudhuri <jishnuraychaudhuri@Jishnus-MacBook-Pro.local>
2025-04-19 12:48:03 -0700

    front page

--- battleshipsApp/src/App.tsx
+++ battleshipsApp/src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import { io, Socket } from 'socket.io-client';
+import './App.css';
+let socket: Socket;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [connected, setConnected] = useState(false);
+
+  useEffect(() => {
+    socket = io('http://localhost:3001');
+
+    socket.on('connect', () => {
+      setConnected(true);
+      console.log('Connected:', socket.id);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className='App'>
+      <h1>Battleships</h1>
+      {connected ? <p>Connected to server</p> : <p>Connecting...</p>}
+    </div>
+  );
 }
 
-export default App
+export default App;
